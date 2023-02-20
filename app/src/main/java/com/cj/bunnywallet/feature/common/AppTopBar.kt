60927e9f93077ec7fc5cfd9ebfc94f0cc167acd7
Claire <wanruaiai@gmail.com>
2023-02-20 08:41:46 +0800

    fix: app top bar click area

--- app/src/main/java/com/cj/bunnywallet/feature/common/AppTopBar.kt
+++ app/src/main/java/com/cj/bunnywallet/feature/common/AppTopBar.kt
@@ -1,24 +1,24 @@
 package com.cj.bunnywallet.feature.common
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.cj.bunnywallet.R
-import com.cj.bunnywallet.ui.theme.Lavender100
 
 @Composable
 fun AppTopBar() {
@@ -27,19 +27,19 @@ fun AppTopBar() {
         modifier = Modifier
             .fillMaxWidth()
             .height(64.dp)
-            .background(color = Lavender100)
-            .zIndex(1f),
     ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_arrow_back),
+        Icon(
+            imageVector = Icons.Rounded.ArrowBack,
             contentDescription = null,
             modifier = Modifier
-                .padding(start = 24.dp)
+                .padding(start = 8.dp)
+                .clip(RoundedCornerShape(16.dp))
                 .clickable {
                     // TODO back action
                 }
                 .align(Alignment.CenterStart)
+                .padding(8.dp)
         )
 
         Text(
@@ -52,7 +52,7 @@ fun AppTopBar() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewAppTopBar() {
     AppTopBar()
