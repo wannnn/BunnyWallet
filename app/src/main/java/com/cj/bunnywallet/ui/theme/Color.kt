ab69cef8344016e530f45b807337b5f7dec72eb2
Claire <wanruaiai@gmail.com>
2023-02-21 07:44:23 +0800

    feat: add common and password text field

--- app/src/main/java/com/cj/bunnywallet/ui/theme/Color.kt
+++ app/src/main/java/com/cj/bunnywallet/ui/theme/Color.kt
@@ -3,10 +3,17 @@ package com.cj.bunnywallet.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Lavender100 = Color(0xFFFBFAFF)
+val Purple40 = Color(0xFF6650a4)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Pink80 = Color(0xFFEFB8C8)
+val Red400 = Color(0xFFFF6565)
+
+val Gray100 = Color(0xFFEFEFF0)
+val Gray200 = Color(0xFFE4E3E6)
+val Gray300 = Color(0xFFD7D7DB)
+val Gray500 = Color(0xFFA1A1AA)
+val Gray600 = Color(0x7FFFFFFF)
+val Gray700 = Color(0xFF484851)
