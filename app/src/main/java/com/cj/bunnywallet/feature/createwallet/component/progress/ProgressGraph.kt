85cbd91ebecbba5a4c47377830c97e03b5c66943
Johnson-OpNet <johnson.tu@opennet.tw>
2023-02-22 09:04:51 +0800

    feat: impl secure wallet screen

--- app/src/main/java/com/cj/bunnywallet/feature/createwallet/component/progress/ProgressGraph.kt
+++ app/src/main/java/com/cj/bunnywallet/feature/createwallet/component/progress/ProgressGraph.kt
@@ -1,14 +1,27 @@
 package com.cj.bunnywallet.feature.createwallet.component.progress
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.cj.bunnywallet.ui.theme.Purple40
+import com.cj.bunnywallet.ui.theme.Purple80
 
 @Composable
 fun ProgressGraph() {
@@ -27,8 +40,53 @@ fun ProgressGraph() {
     }
 }
 
+
+private val outlined = Modifier
+    .clip(CircleShape)
+    .border(2.dp, Purple40, CircleShape)
+
+private val filled = Modifier
+    .background(Purple40, shape = CircleShape)
+
+@Composable
+private fun ProgressBall(step: String, hasDone: Boolean) {
+    Text(
+        text = step,
+        modifier = Modifier
+            .then((if (hasDone) filled else outlined))
+            .size(24.dp)
+            .wrapContentHeight(),
+        color = if (hasDone) Color.White else Color.Black,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+private fun RowScope.ProgressLine(hasDone: Boolean) {
+    Divider(
+        modifier = Modifier.weight(1f, true),
+        thickness = 2.dp,
+        color = if (hasDone) Purple40 else Purple80,
+    )
+}
+
+
 @Preview(showBackground = true, widthDp = 400)
 @Composable
 fun PreviewProgressGraph() {
     ProgressGraph()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProgressBall() {
+    ProgressBall("1", false)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProgressLine() {
+    Row {
+        ProgressLine(true)
+    }
 }
---
+++ app/src/main/java/com/cj/bunnywallet/feature/createwallet/securewallet/component/SecureSuggestion.kt
@@ -0,0 +1,98 @@
+package com.cj.bunnywallet.feature.createwallet.securewallet.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cj.bunnywallet.R
+import com.cj.bunnywallet.ui.theme.Purple40
+
+@Composable
+fun SecureSuggestions() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        Suggestion(
+            R.drawable.ic_edit_document,
+            R.string.write_down_title,
+            R.string.write_down_content,
+        )
+        Suggestion(
+            R.drawable.ic_cloud_done,
+            R.string.store_digitally_title,
+            R.string.store_digitally_content,
+        )
+        Suggestion(
+            R.drawable.ic_dynamic_feed,
+            R.string.split_title,
+            R.string.split_content,
+        )
+        Suggestion(
+            R.drawable.ic_cognition,
+            R.string.memorize_title,
+            R.string.memorize_content,
+        )
+    }
+}
+
+@Composable
+private fun Suggestion(icon: Int, title: Int, content: Int) {
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            tint = Purple40,
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            Text(
+                text = stringResource(id = title),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
+
+            Text(
+                text = stringResource(id = content),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 400)
+@Composable
+fun PreviewSecureSuggestions() {
+    SecureSuggestions()
+}
+
+@Preview(showBackground = true, widthDp = 400)
+@Composable
+fun PreviewSuggestion() {
+    Suggestion(
+        R.drawable.ic_edit_document,
+        R.string.write_down_title,
+        R.string.write_down_content,
+    )
+}
