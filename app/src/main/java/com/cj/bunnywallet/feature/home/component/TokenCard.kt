26d98ffb8f3fe306a4ca6396e11858bcfcc62c19
Claire <wanruaiai@gmail.com>
2023-04-02 23:53:50 +0800

    feat: token card ui

---
+++ app/src/main/java/com/cj/bunnywallet/feature/home/component/TokenCard.kt
@@ -0,0 +1,84 @@
+package com.cj.bunnywallet.feature.home.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cj.bunnywallet.R
+import com.cj.bunnywallet.ui.theme.BunnyWalletTheme
+
+@Composable
+fun TokenCard() {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(color = MaterialTheme.colorScheme.inversePrimary)
+            .clickable {  }
+            .padding(15.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_bunny),
+            contentDescription = null,
+            modifier = Modifier
+                .weight(1f)
+                .aspectRatio(1f)
+        )
+
+        Text(
+            text = "Bunny USDT",
+            modifier = Modifier
+                .padding(start = 12.dp)
+                .weight(weight = 3f),
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Column(
+            modifier = Modifier.weight(weight = 5f),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "50.5555",
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                textAlign = TextAlign.End
+            )
+
+            Text(
+                text = "≈ 0.05 USD",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.End
+            )
+        }
+    }
+}
+
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun PreviewTokenCard() {
+    BunnyWalletTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            TokenCard()
+        }
+    }
+}
--- app/src/main/java/com/cj/bunnywallet/feature/home/component/TokensList.kt
+++ app/src/main/java/com/cj/bunnywallet/feature/home/component/TokensList.kt
@@ -1,110 +1,42 @@
 package com.cj.bunnywallet.feature.home.component
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cj.bunnywallet.R
-import com.cj.bunnywallet.feature.common.CommonIconBtn
 import com.cj.bunnywallet.feature.home.HomeState
-import com.cj.bunnywallet.ui.customview.VisibilityIconBtn
 import com.cj.bunnywallet.ui.theme.BunnyWalletTheme
 
 @Composable
 fun TokensList(uiState: HomeState) {
     var balanceVisibility by remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        BalanceInfo(
-            balance = uiState.balance,
-            balanceVisibility = balanceVisibility,
-            onVisibilityChange = { balanceVisibility = balanceVisibility.not() }
-        )
-    }
-}
-
-@Composable
-fun BalanceInfo(
-    balance: String,
-    balanceVisibility: Boolean,
-    onVisibilityChange: () -> Unit
-) {
-    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = stringResource(id = R.string.assets),
-                modifier = Modifier
-                    .clip(RoundedCornerShape(5.dp))
-                    .background(color = MaterialTheme.colorScheme.inverseOnSurface)
-                    .padding(vertical = 4.dp, horizontal = 8.dp),
-                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+        item {
+            AssetsInfo(
+                balance = uiState.balance,
+                balanceVisibility = balanceVisibility,
+                onVisibilityChange = { balanceVisibility = balanceVisibility.not() }
             )
-
-            Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                val bgModifier = Modifier.background(
-                    color = MaterialTheme.colorScheme.inverseOnSurface,
-                    shape = CircleShape
-                )
-
-                CommonIconBtn(
-                    icon = R.drawable.ic_create,
-                    modifier = bgModifier,
-                    onClick = { }
-                )
-
-                CommonIconBtn(
-                    icon = R.drawable.ic_add,
-                    modifier = bgModifier,
-                    onClick = { }
-                )
-            }
         }
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            VisibilityIconBtn(
-                isVisible = balanceVisibility,
-                onVisibilityChange = onVisibilityChange
-            )
-
-            val currency = stringResource(id = R.string.usd)
-            Text(
-                text = balance.plus(" ").plus(currency),
-                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
-            )
+        items(count = 10) {
+            TokenCard()
         }
     }
 }
