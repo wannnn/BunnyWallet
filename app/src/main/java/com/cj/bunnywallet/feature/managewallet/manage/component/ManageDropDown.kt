f4880d80a13b7ea7a8c7912909ea7b34265a1612
Johnson-OpNet <johnson.tu@opennet.tw>
2023-04-09 13:51:55 +0800

    feat: add manage wallet dropdown

---
+++ app/src/main/java/com/cj/bunnywallet/feature/managewallet/manage/component/ManageDropDown.kt
@@ -0,0 +1,54 @@
+package com.cj.bunnywallet.feature.managewallet.manage.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import com.cj.bunnywallet.R
+import com.cj.bunnywallet.feature.common.CommonIconBtn
+import com.cj.bunnywallet.feature.managewallet.manage.type.ManageType
+
+@Composable
+fun BoxScope.ManageDropDown() {
+    var dmExpand by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.align(Alignment.CenterEnd)) {
+        CommonIconBtn(
+            icon = R.drawable.ic_wallet,
+            modifier = Modifier
+                .padding(end = 8.dp),
+            onClick = { dmExpand = true }
+        )
+
+        DropdownMenu(
+            expanded = dmExpand,
+            onDismissRequest = { dmExpand = false },
+            modifier = Modifier.align(Alignment.TopEnd),
+            offset = DpOffset(x = 12.dp, y = 0.dp),
+        ) {
+            ManageType.values().forEach {
+                DropdownMenuItem(
+                    text = { Text(text = stringResource(id = it.typeName)) },
+                    onClick = { },
+                    leadingIcon = {
+                        Icon(painter = painterResource(id = it.icon), contentDescription = null)
+                    },
+                )
+            }
+        }
+    }
+}
